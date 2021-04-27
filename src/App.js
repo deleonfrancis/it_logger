559a8abbb4fdb156208a35d645beaf270938497c
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-27 12:03:25 -0400

    created search bar component and put it on the page.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 
 import "./App.css";
+import SearchBar from "./components/layout/SearchBar";
 
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   }, []);
-  return <div className="App">My App</div>;
+  return <div className="App">
+    <SearchBar />
+  </div>;
 };
 
 export default App;
