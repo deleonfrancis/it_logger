3953d74eb104fbcc9198714d1c5aaecd53f2d4ec
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-27 15:13:11 -0400

    added store index.js reducer. set up for redux

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { useEffect } from "react";
-import "materialize-css/dist/css/materialize.min.css";
-import M from "materialize-css/dist/js/materialize.min.js";
 
-import "./App.css";
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/log/Logs";
 import AddBtn from "./components/layout/AddBtn";
@@ -11,23 +8,32 @@ import EditLogModal from "./components/log/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
 
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
+import "./App.css";
+
+import { Provider } from "react-redux";
+import store from "./store";
+
 const App = () => {
   useEffect(() => {
     // Init Materialize JS
     M.AutoInit();
   });
   return (
-    <div className="App">
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal />
-        <TechListModal />
-        <Logs />
+    <Provider store={store}>
+      <div className="App">
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 };
 
